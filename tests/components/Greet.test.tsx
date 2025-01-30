4c2e31bfb1998004960016c02a4c2e86f293ba02
samuel <adeyemis710@gmail.com>
2025-01-30 10:43:28 +0100

    added vitest

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,12 +1,12 @@
 import { it, expect, describe } from 'vitest';
 import {render, screen} from '@testing-library/react';
 import Greet from '../../src/components/Greet';
-
+import '@testing-library/jest-dom/vitest';
 
 describe('Greet', () => {
   it('should render Hello with the name when name is provided', () => {
     render(<Greet name='Sam' />);
-    screen.debug();
-    
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument();
   })  
 })
