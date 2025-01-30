bb0ed64950c04c03035135c4f1fc63df979b9425
samuel <adeyemis710@gmail.com>
2025-01-30 10:39:16 +0100

    added vitest

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,12 @@
+import { it, expect, describe } from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Greet from '../../src/components/Greet';
+
+
+describe('Greet', () => {
+  it('should render Hello with the name when name is provided', () => {
+    render(<Greet name='Sam' />);
+    screen.debug();
+    
+  })  
+})
